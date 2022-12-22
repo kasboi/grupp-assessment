562e1826eebcf51dd2f540408221c6071aff53ab
Abdullah Solahudeen <solahudeen15@gmail.com>
2022-12-22 07:43:00 +0100

    init repo

---
+++ src/Navbar/Icon.jsx
@@ -0,0 +1,120 @@
+const LayerIcon = () => {
+    return (
+        <svg
+            width="18"
+            height="18"
+            viewBox="0 0 22 22"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <path
+                d="M1 16L11 21L21 16M1 11L11 16L21 11M11 1L1 6L11 11L21 6L11 1Z"
+                stroke="#667085"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+            />
+        </svg>
+    )
+}
+const ChartIcon = () => {
+    return (
+        <svg
+            width="18"
+            height="18"
+            viewBox="0 0 14 18"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <path
+                d="M13 17V7M7 17V1M1 17V11"
+                stroke="#667085"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+            />
+        </svg>
+    )
+}
+
+const HomeIcon = () => {
+    return (
+        <svg
+            width="18"
+            height="18"
+            viewBox="0 0 20 22"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <path
+                d="M7 21V11H13V21M1 8L10 1L19 8V19C19 19.5304 18.7893 20.0391 18.4142 20.4142C18.0391 20.7893 17.5304 21 17 21H3C2.46957 21 1.96086 20.7893 1.58579 20.4142C1.21071 20.0391 1 19.5304 1 19V8Z"
+                stroke="#667085"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+            />
+        </svg>
+    )
+}
+
+const TaskIcon = () => {
+    return (
+        <svg
+            width="18"
+            height="18"
+            viewBox="0 0 21 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <path
+                d="M7 9L10 12L20 2M19 10V17C19 17.5304 18.7893 18.0391 18.4142 18.4142C18.0391 18.7893 17.5304 19 17 19H3C2.46957 19 1.96086 18.7893 1.58579 18.4142C1.21071 18.0391 1 17.5304 1 17V3C1 2.46957 1.21071 1.96086 1.58579 1.58579C1.96086 1.21071 2.46957 1 3 1H14"
+                stroke="#667085"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+            />
+        </svg>
+    )
+}
+
+const FlagIcon = () => {
+    return (
+        <svg
+            width="18"
+            height="18"
+            viewBox="0 0 18 22"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <path
+                d="M1 14C1 14 2 13 5 13C8 13 10 15 13 15C16 15 17 14 17 14V2C17 2 16 3 13 3C10 3 8 1 5 1C2 1 1 2 1 2V14ZM1 14V21"
+                stroke="#667085"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+            />
+        </svg>
+    )
+}
+
+const UserIcon = () => {
+    return (
+        <svg
+            width="18"
+            height="18"
+            viewBox="0 0 24 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <path
+                d="M17 19V17C17 15.9391 16.5786 14.9217 15.8284 14.1716C15.0783 13.4214 14.0609 13 13 13H5C3.93913 13 2.92172 13.4214 2.17157 14.1716C1.42143 14.9217 1 15.9391 1 17V19M23 19V17C22.9993 16.1137 22.7044 15.2528 22.1614 14.5523C21.6184 13.8519 20.8581 13.3516 20 13.13M16 1.13C16.8604 1.3503 17.623 1.8507 18.1676 2.55231C18.7122 3.25392 19.0078 4.11683 19.0078 5.005C19.0078 5.89317 18.7122 6.75608 18.1676 7.45769C17.623 8.1593 16.8604 8.6597 16 8.88M13 5C13 7.20914 11.2091 9 9 9C6.79086 9 5 7.20914 5 5C5 2.79086 6.79086 1 9 1C11.2091 1 13 2.79086 13 5Z"
+                stroke="#667085"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+            />
+        </svg>
+    )
+}
+
+export { LayerIcon, ChartIcon, HomeIcon, TaskIcon, FlagIcon, UserIcon }
---
+++ src/Navbar/Navbar.jsx
@@ -0,0 +1,64 @@
+import { MagnifyingGlassIcon, ChartBarIcon } from "@heroicons/react/20/solid"
+import {
+    ChartIcon,
+    FlagIcon,
+    HomeIcon,
+    LayerIcon,
+    TaskIcon,
+    UserIcon,
+} from "./Icon"
+
+const navList = [
+    {
+        name: "Home",
+        icon: <HomeIcon />,
+    },
+    {
+        name: "Dashboard",
+        icon: <ChartIcon />,
+    },
+    {
+        name: "Projects",
+        icon: <LayerIcon />,
+    },
+    {
+        name: "Tasks",
+        icon: <TaskIcon />,
+    },
+    {
+        name: "Reporting",
+        icon: <FlagIcon />,
+    },
+    {
+        name: "Users",
+        icon: <UserIcon />,
+    },
+]
+
+const Navbar = () => {
+    return (
+        <div className="mx-4">
+            <p className="text-2xl">Untitled UI</p>
+            <div className="relative max-w-sm mx-auto">
+                <input
+                    type="text"
+                    name="search"
+                    id="search"
+                    placeholder="Search"
+                    className="border border-slate-300 outline-blue-500 w-full rounded-lg pl-10 pr-2 py-2"
+                />
+                <MagnifyingGlassIcon className="w-5 absolute top-2.5 left-2 text-gray-400" />
+            </div>
+            <div className="mt-8 text-gray-700">
+                {navList.map((item) => (
+                    <div className="flex items-center mb-2 p-0.5 hover:bg-slate-300">
+                        <div className="mr-3">{item.icon}</div>
+                        <span className="text-md">{item.name}</span>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
