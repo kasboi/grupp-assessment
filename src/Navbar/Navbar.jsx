a8cb1809024096e2ed5276c6cd7437ceec67724c
Abdullah Solahudeen <solahudeen15@gmail.com>
2022-12-22 13:08:56 +0100

    added navbar footer

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,12 +1,19 @@
-import { MagnifyingGlassIcon, ChartBarIcon } from "@heroicons/react/20/solid"
+import { Logo } from "./../components/Logo"
+import { Search } from "./../components/Search"
+
 import {
     ChartIcon,
     FlagIcon,
     HomeIcon,
     LayerIcon,
+    SettingsIcon,
+    SupportIcon,
     TaskIcon,
     UserIcon,
 } from "./Icon"
+
+import logoImg from "../assets/Logomark.svg"
+import imgWrap from "../assets/ImageWrap.svg"
 
 const navList = [
     {
@@ -37,27 +44,52 @@ const navList = [
 
 const Navbar = () => {
     return (
-        <div className="mx-4">
-            <p className="text-2xl">Untitled UI</p>
-            <div className="relative max-w-sm mx-auto">
-                <input
-                    type="text"
-                    name="search"
-                    id="search"
-                    placeholder="Search"
-                    className="border border-slate-300 outline-blue-500 w-full rounded-lg pl-10 pr-2 py-2"
-                />
-                <MagnifyingGlassIcon className="w-5 absolute top-2.5 left-2 text-gray-400" />
-            </div>
-            <div className="mt-8 text-gray-700">
+        <div className="px-6">
+            <Logo Logo={logoImg} />
+            <Search />
+            <div className="mt-4 mb-7">
                 {navList.map((item) => (
-                    <div className="flex items-center mb-2 p-0.5 hover:bg-slate-300">
-                        <div className="mr-3">{item.icon}</div>
-                        <span className="text-md">{item.name}</span>
-                    </div>
+                    <button className="flex items-center mb-1.5 py-1 pl-0.5 w-full rounded-md hover:bg-slate-100">
+                        <div className="mr-3 text-gray-900">{item.icon}</div>
+                        <span className="text-md text-gray-700">
+                            {item.name}
+                        </span>
+                    </button>
                 ))}
             </div>
-            
+            <div className="mb-4 text-gray-700">
+                <div className="mb-4">
+                    <button className="flex w-full items-center mb-2 p-0.5 hover:bg-slate-100">
+                        <div className="mr-3">
+                            <SupportIcon />
+                        </div>
+                        <span className="text-md">Support</span>
+                    </button>
+                    <button className="flex w-full items-center mb-2 p-0.5 hover:bg-slate-100">
+                        <div className="mr-3">
+                            <SettingsIcon />
+                        </div>
+                        <span className="text-md">Settings</span>
+                    </button>
+                </div>
+                <div className="bg-slate-100 rounded-xl px-7 py-6 max-w-sm">
+                    <h3 className="font-semibold text-xl text-black mb-0.5">
+                        New features available!
+                    </h3>
+                    <p className="text-lg py-1 mb-3">
+                        Check out the new dashboard view. Pages now load faster.{" "}
+                    </p>
+                    <img
+                        src={imgWrap}
+                        alt="video_placeholder"
+                        className="w-full mb-4"
+                    />
+                    <div>
+                        <button className="mr-5">Dismiss</button>
+                        <button className="text-violet-800">What's new?</button>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
