92aeccc1b0c18cfffcb09bb9ec3a9e260b4de0b6
Abdullah Solahudeen <solahudeen15@gmail.com>
2022-12-25 10:48:24 +0100

    replace table pictures with original

--- src/SideBar/SideBar.jsx
+++ src/SideBar/SideBar.jsx
@@ -16,7 +16,7 @@ import {
 
 import logoImg from "../assets/Logomark.svg"
 import imgWrap from "../assets/ImageWrap.svg"
-import avatar from "../assets/Avatar.svg"
+import avatar from "../assets/Avatar1.svg"
 import NavBar from "../NavBar/NavBar"
 
 const navList = [
@@ -49,9 +49,9 @@ const navList = [
 const SideBar = ({ visible, setVisible }) => {
     return (
         <div
-            className={`px-6 py-2 z-20 h-full bg-slate-50 ${
+            className={`px-4 py-2 z-20 h-full bg-slate-50 ${
                 visible ? "absolute" : "hidden"
-            } top-0 left-0 lg:block lg:relative lg:basis-1/3`}
+            } top-0 left-0 lg:block lg:relative`}
         >
             <div className="flex justify-between items-center">
                 <Logo Logo={logoImg} />
@@ -95,11 +95,11 @@ const SideBar = ({ visible, setVisible }) => {
                         <span className="text-md">Settings</span>
                     </button>
                 </div>
-                <div className="bg-slate-100 rounded-xl px-7 py-6 mb-10 max-w-sm">
+                <div className="bg-slate-100 rounded-xl px-6 py-6 mb-10 max-w-xs">
                     <h3 className="font-semibold text-xl text-black mb-0.5">
                         New features available!
                     </h3>
-                    <p className="text-lg py-1 mb-3">
+                    <p className="text-base sm:text-lg py-1 mb-3">
                         Check out the new dashboard view. Pages now load faster.{" "}
                     </p>
                     <img
@@ -114,7 +114,7 @@ const SideBar = ({ visible, setVisible }) => {
                 </div>
             </div>
             <div className="flex items-center justify-evenly py-4 border-t-2">
-                <img src={avatar} alt="avatar" />
+                <img src={avatar} alt="avatar" className="h-12 w-12 sm:h-16 sm:w-16"/>
                 <div>
                     <h3>Olivia Rhye</h3>
                     <p className="text-gray-600">olivia@untitledui.com</p>
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,42 +1,42 @@
-import AvatarOne from "./Avatars"
 import { DownloadIcon } from "./Icon"
-
+import Avatar from "../assets/Avatar.svg"
+import { AvatarFive, AvatarFour, AvatarThree, AvatarTwo, AvatarOne } from "./Avatars"
 
 const tableDetails = [
     {
         invoice: "Basic Plan - Dec 2022",
         date: "Dec 1, 2022",
-
+        pic: <AvatarFive />,
     },
     {
         invoice: "Basic Plan - Nov 2022",
         date: "Nov 1, 2022",
-
+        pic: <AvatarFour />,
     },
     {
         invoice: "Basic Plan - Oct 2022",
         date: "Oct 1, 2022",
-
+        pic: <AvatarThree />,
     },
     {
         invoice: "Basic Plan - Sep 2022",
         date: "Sep 1, 2022",
-
+        pic: <AvatarOne />,
     },
     {
         invoice: "Basic Plan - Aug 2022",
         date: "Aug 1, 2022",
-
+        pic: <AvatarTwo />,
     },
     {
         invoice: "Basic Plan - Jul 2022",
         date: "Jul 1, 2022",
-
+        pic: <AvatarTwo />,
     },
     {
         invoice: "Basic Plan - Jun 2022",
         date: "Jun 1, 2022",
-
+        pic: <AvatarOne />,
     },
 ]
 
@@ -103,59 +103,61 @@ const Table = () => {
                                     </tr>
                                 </thead>
                                 <tbody className="bg-white divide-y divide-gray-200">
-                                { tableDetails.map(item => (
-                                    <tr className="hover:bg-gray-100">
-                                        <td className="p-4 w-4">
-                                            <div className="flex items-center">
-                                                <input
-                                                    id="checkbox-table-1"
-                                                    type="checkbox"
-                                                    className="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 focus:ring-2"
-                                                />
-                                                <label
-                                                    htmlFor="checkbox-table-1"
-                                                    className="sr-only"
-                                                >
-                                                    checkbox
-                                                </label>
-                                            </div>
-                                        </td>
-                                        <td className="py-4 px-6 w-full text-sm font-medium text-gray-900 whitespace-nowrap">
-                                            {item.invoice}
-                                        </td>
-                                        <td className="py-4 px-6 text-sm font-medium text-gray-500 whitespace-nowrap">
-                                            USD $10.00
-                                        </td>
-                                        <td className="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap">
-                                            {item.date}
-                                        </td>
-                                        <td className="w-4">
-                                            <span className="px-2 py-1.5 flex items-center justify-center text-base rounded-full text-green-600  bg-green-200 ">
-                                                <svg
-                                                    xmlns="http://www.w3.org/2000/svg"
-                                                    fill="none"
-                                                    viewBox="0 0 24 24"
-                                                    stroke-width="2.5"
-                                                    stroke="currentColor"
-                                                    class="w-4 h-4 mr-2"
-                                                >
-                                                    <path
-                                                        stroke-linecap="round"
-                                                        stroke-linejoin="round"
-                                                        d="M4.5 12.75l6 6 9-13.5"
+                                    {tableDetails.map((item) => (
+                                        <tr className="hover:bg-gray-100">
+                                            <td className="p-4 w-4">
+                                                <div className="flex items-center">
+                                                    <input
+                                                        id="checkbox-table-1"
+                                                        type="checkbox"
+                                                        className="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 focus:ring-2"
                                                     />
-                                                </svg>
-                                                <span className="text-sm">Paid</span>
-                                            </span>
-                                        </td>
-                                        <td className="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap">
-                                            <AvatarOne />
-                                        </td>
-                                        <td className="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap">
-                                            <DownloadIcon />
-                                        </td>
-                                    </tr>
-                                ))}
+                                                    <label
+                                                        htmlFor="checkbox-table-1"
+                                                        className="sr-only"
+                                                    >
+                                                        checkbox
+                                                    </label>
+                                                </div>
+                                            </td>
+                                            <td className="py-4 px-6 w-full text-sm font-medium text-gray-900 whitespace-nowrap">
+                                                {item.invoice}
+                                            </td>
+                                            <td className="py-4 px-6 text-sm font-medium text-gray-500 whitespace-nowrap">
+                                                USD $10.00
+                                            </td>
+                                            <td className="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap">
+                                                {item.date}
+                                            </td>
+                                            <td className="w-4">
+                                                <span className="px-1 py-1 flex items-center justify-center text-base rounded-full text-green-600  bg-green-200 ">
+                                                    <svg
+                                                        xmlns="http://www.w3.org/2000/svg"
+                                                        fill="none"
+                                                        viewBox="0 0 24 24"
+                                                        stroke-width="2.5"
+                                                        stroke="currentColor"
+                                                        class="w-4 h-4 mr-2"
+                                                    >
+                                                        <path
+                                                            stroke-linecap="round"
+                                                            stroke-linejoin="round"
+                                                            d="M4.5 12.75l6 6 9-13.5"
+                                                        />
+                                                    </svg>
+                                                    <span className="text-sm">
+                                                        Paid
+                                                    </span>
+                                                </span>
+                                            </td>
+                                            <td className="py-2 px-4 text-sm font-medium text-gray-900 whitespace-nowrap">
+                                                {item.pic}
+                                            </td>
+                                            <td className="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap">
+                                                <DownloadIcon />
+                                            </td>
+                                        </tr>
+                                    ))}
                                 </tbody>
                             </table>
                         </div>
