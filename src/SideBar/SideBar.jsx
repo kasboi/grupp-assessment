fd3206bfa2af02bf34ebb867a2190ea237555f3c
Abdullah Solahudeen <solahudeen15@gmail.com>
2022-12-25 12:02:15 +0100

    fix dashboard badge

--- src/SideBar/SideBar.jsx
+++ src/SideBar/SideBar.jsx
@@ -27,6 +27,7 @@ const navList = [
     {
         name: "Dashboard",
         icon: <ChartIcon />,
+        badge: true,
     },
     {
         name: "Projects",
@@ -74,9 +75,14 @@ const SideBar = ({ visible, setVisible }) => {
                         className="flex items-center mb-1.5 py-2 pl-0.5 w-full rounded-md hover:bg-slate-100"
                     >
                         <div className="mr-3 text-gray-900">{item.icon}</div>
-                        <span className="text-md text-gray-700">
+                        <span className="text-md text-gray-700 mr-auto">
                             {item.name}
                         </span>
+                        {item.badge && (
+                            <span className="bg-slate-200 px-2.5 py-0.5 rounded-full">
+                                10
+                            </span>
+                        )}
                     </button>
                 ))}
             </div>
@@ -114,7 +120,11 @@ const SideBar = ({ visible, setVisible }) => {
                 </div>
             </div>
             <div className="flex items-center justify-evenly py-4 border-t-2">
-                <img src={avatar} alt="avatar" className="h-12 w-12 sm:h-16 sm:w-16"/>
+                <img
+                    src={avatar}
+                    alt="avatar"
+                    className="h-12 w-12 sm:h-16 sm:w-16"
+                />
                 <div>
                     <h3>Olivia Rhye</h3>
                     <p className="text-gray-600">olivia@untitledui.com</p>
