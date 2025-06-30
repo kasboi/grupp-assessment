6160f71e98dde53c3f1a67297a2977302aab76e2
Abdullah <solahudeen15@gmail.com>
2025-06-30 10:37:09 +0100

    typo fix

--- src/NavBar/NavBar.jsx
+++ src/NavBar/NavBar.jsx
@@ -1,22 +1,20 @@
-import { Logo } from "../components/Logo"
+import { Logo } from "../components/Logo";
 
-import logoImg from "../assets/Logomark.svg"
-import { NavIcon } from "../components/Icon"
+import logoImg from "../assets/Logomark.svg";
+import { NavIcon } from "../components/Icon";
 
 const NavBar = ({ visible, setVisible }) => {
-    return (
-        <div className="lg:hidden px-6 flex justify-between items-center">
-            <Logo Logo={logoImg} />
-            <button
-                className={`${
-                    visible ? "hidden" : "inline-block"
-                } sm:inline-block`}
-                onClick={() => setVisible(!visible)}
-            >
-                <NavIcon />
-            </button>
-        </div>
-    )
-}
+  return (
+    <div className="lg:hidden px-6 flex justify-between items-center">
+      <Logo Logo={logoImg} />
+      <button
+        className={`${visible ? "hidden" : "inline-block"} sm:inline-block`}
+        onClick={() => setVisible(!visible)}
+      >
+        <NavIcon />
+      </button>
+    </div>
+  );
+};
 
-export default NavBar
+export default NavBar;
