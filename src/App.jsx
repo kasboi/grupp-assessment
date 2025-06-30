06cc3a603a50d9b98557f4e38e08852f035fbbc0
Abdullah <solahudeen15@gmail.com>
2025-06-30 10:36:34 +0100

    typo fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
-import SideBar from "./SideBar"
-import Settings from "./Settings"
-import NavBar from "./NavBar/NavBar"
-import { useState } from "react"
+import SideBar from "./SideBar";
+import Settings from "./Settings";
+import NavBar from "./NavBar/NavBar";
+import { useState } from "react";
 
 function App() {
-    const [visible, setVisible] = useState(false)
+  const [visible, setVisible] = useState(false);
 
-    return (
-        <div className="App relative overflow-hidden">
-            <NavBar visible={visible} setVisible={setVisible}/>
-            <div className="flex w-full">
-                <SideBar visible={visible} setVisible={setVisible}/>
-                <Settings setVisible={setVisible}/>
-            </div>
-        </div>
-    )
+  return (
+    <div className="App relative overflow-hidden">
+      <NavBar visible={visible} setVisible={setVisible} />
+      <div className="flex w-full">
+        <SideBar visible={visible} setVisible={setVisible} />
+        <Settings setVisible={setVisible} />
+      </div>
+    </div>
+  );
 }
-
-export default App
+export default App;
